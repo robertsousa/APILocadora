e7265217fa964b77527fa908575fb51070ace561
Roberto Sousa <robert6257@gmail.com>
2022-02-28 17:08:42 -0300

    Adicionado o IClienteService

---
+++ LocadoraAPI/Services/IClienteService.cs
@@ -0,0 +1,14 @@
+﻿using LocadoraAPI.Models;
+
+namespace LocadoraAPI.Services
+{
+    public interface IClienteService
+    {
+        Cliente AddCliente(Cliente cliente);
+        Cliente FindByID(int id);
+        Cliente FindByName(string name);
+        List<Cliente> GetAll();
+        void DeleteCliente(int id); 
+
+    }
+}
