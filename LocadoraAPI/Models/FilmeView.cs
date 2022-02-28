2a920b87c5489a04def79f155383b1f12f9747af
Roberto Sousa <robert6257@gmail.com>
2022-02-28 18:37:58 -0300

    Adicionado o modelo filme

---
+++ LocadoraAPI/Models/FilmeView.cs
@@ -0,0 +1,9 @@
+﻿namespace LocadoraAPI.Models
+{
+    public class FilmeView
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsLeased { get; set; }
+    }
+}
