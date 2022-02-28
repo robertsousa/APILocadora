63e960a831794ed6bfd557cb0d2c87545bfeba10
Roberto Sousa <robert6257@gmail.com>
2022-02-28 17:04:47 -0300

    Criado o clienteController

--- LocadoraAPI/Controllers/ClienteController.cs
+++ LocadoraAPI/Controllers/ClienteController.cs
@@ -5,12 +5,7 @@ namespace LocadoraAPI.Controllers
     [ApiController]
     [Route("[controller]")]
     public class ClienteController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+    {        
         private readonly ILogger<ClienteController> _logger;
 
         public ClienteController(ILogger<ClienteController> logger)
@@ -18,16 +13,11 @@ namespace LocadoraAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("get-all")]
+        public IActionResult GetAllCliente()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+
+            return Ok();
         }
     }
 }
