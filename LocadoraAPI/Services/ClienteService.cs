9ae57dd2fec1091769743f9bde3cf817f35bf90b
Roberto Sousa <robert6257@gmail.com>
2022-02-28 17:10:17 -0300

    Criado a implementacao ClienteService

---
+++ LocadoraAPI/Services/ClienteService.cs
@@ -0,0 +1,32 @@
+﻿using LocadoraAPI.Models;
+
+namespace LocadoraAPI.Services
+{
+    public class ClienteService : IClienteService
+    {
+        public Cliente AddCliente(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCliente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
