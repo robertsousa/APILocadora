de9ff6a5cbc9f78bc27e6afec1bfc83df7c5ba6f
Roberto Sousa <robert6257@gmail.com>
2022-02-28 16:54:52 -0300

    Criado o model cliente

---
+++ LocadoraAPI/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace LocadoraAPI.Models
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true; 
+    }
+}
